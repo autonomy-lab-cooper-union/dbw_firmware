ff57d6fdcd87d7e56402beba9ffba83bb23476d1
dmezh <54985569+dmezh@users.noreply.github.com>
2021-02-20 17:32:52 -0500

    Move CAN message definitions to enum

--- test_espidf/include/MessageID.h
+++ test_espidf/include/MessageID.h
@@ -1,13 +1,17 @@
 #ifndef MESSAGE_ID_H
 #define MESSAGE_ID_H
 
-#define ESTOP_RANGE_START 1024
-#define ESTOP_RANGE_END 2047
 //if (rx_frame.MsgID >= ESTOP_RANGE_START && rx_frame.MsgID <= ESTOP_RANGE_END) set queue to canToEStop
-#define HOUSE_RANGE_START 2048
-#define HOUSE_RANGE_END 3071
-#define WATCH_RANGE_START 3072
-#define WATCH_RANGE_END 4095
+
+enum CAN_MSGS {
+    ESTOP_RANGE_START   = 0x400,
+    ESTOP_RANGE_END     = 0x7FF,
+    HOUSE_RANGE_START   = 0x800,
+    HOUSE_RANGE_END     = 0xBFF,
+    WATCH_RANGE_START   = 0xC00,
+    WATCH_RANGE_END     = 0xFFF
+};
+
 enum es_actions {e_stop = ESTOP_RANGE_START, last_estop_action = ESTOP_RANGE_END};
 enum h_actions {first_h_action = HOUSE_RANGE_START};
 enum w_actions {first_w_action = WATCH_RANGE_START};
