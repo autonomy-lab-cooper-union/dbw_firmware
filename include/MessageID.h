cf24cec17f33e41a2fa7ed8706b95df3a0eb6a08
kevinjiang11579 <jiang9@cooper.edu>
2021-02-25 20:55:59 -0500

    Added NodeIDs to MessageID.h

--- include/MessageID.h
+++ include/MessageID.h
@@ -3,22 +3,40 @@
 
 //if (rx_frame.MsgID >= ESTOP_RANGE_START && rx_frame.MsgID <= ESTOP_RANGE_END) set queue to canToEStop
 
-enum CAN_MSGS {
+enum taskID  {
+    SPCL_RANGE_START = 0x000,
+    #ifdef BRAKE
+        //brake specific task names here
+    #elif STEERING
+        //steering specific task names here
+    #elif SPEED
+        //speed specific task names here
+    #elif MISC
+        //miscellaneous specific task names here
+    #endif
+    SPCL_RANGE_END = 0x3FF,
     ESTOP_RANGE_START   = 0x400,
+    e_stop,
     ESTOP_RANGE_END     = 0x7FF,
     HOUSE_RANGE_START   = 0x800,
     HOUSE_RANGE_END     = 0xBFF,
     WATCH_RANGE_START   = 0xC00,
     WATCH_RANGE_END     = 0xFFF
 };
-
-enum es_actions {e_stop = ESTOP_RANGE_START, last_estop_action = ESTOP_RANGE_END};
-enum h_actions {first_h_action = HOUSE_RANGE_START};
-enum w_actions {first_w_action = WATCH_RANGE_START};
 //Within Estoptask
 //es_actions temp = rx_frame.MsgID;
 //switch(temp){case e_stop: ...}
 //get the message id and see if it lies in a certain range. Estop task looks at the number and matches it to some action. 
+enum nodeID {
+    BRK_RANGE_START = 0x000,
+    BRK_RANGE_END = 0x3FF,
+    STR_RANGE_START = 0x400,
+    STR_RANGE_END = 0x7FF,
+    SPD_RANGE_START = 0x800,
+    SPD_RANGE_END = 0xBFF,
+    MSC_RANGE_START = 0xC00,
+    MSC_RANGE_END = 0xFFF
+};
 
 
 #endif
